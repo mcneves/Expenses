5bb681e9d3f10f27f0ef976257a516b276b94006
mcneves <mcn@isep.ipp.pt>
2013-05-12 13:50:54 +0100

    Classe JpaGeneric is a copy of  JpaRepository created by Paulo Sousa

--- ExpenseManagerSimple/src/persistence/jpa/JpaGeneric.java
+++ ExpenseManagerSimple/src/persistence/jpa/JpaGeneric.java
@@ -4,7 +4,7 @@
  */
 package persistence.jpa;
 
-import Model.ExpenseType;
+
 import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
 import java.util.Collection;
@@ -14,128 +14,146 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.persistence.PersistenceException;
+import javax.persistence.PersistenceUnit;
 import javax.persistence.Query;
+
 
 /**
  *
- * @author mcn
+ * @author Paulo Gandra Sousa
+ *
+ * based on
+ * http://stackoverflow.com/questions/3888575/single-dao-generic-crud-methods-jpa-hibernate-spring
+ * and on https://burtbeckwith.com/blog/?p=40
  */
+
+// original class name was JpaRepository
 public class JpaGeneric<T, PK extends Serializable> {
 
-      static protected EntityManagerFactory emf =
-              Persistence.createEntityManagerFactory("ExpenseManagerSimplePU");
+      @PersistenceUnit
+    static protected EntityManagerFactory emf = Persistence.createEntityManagerFactory("ExpenseManagerSimplePU");
+    EntityManager entityManager; // = emf.createEntityManager();
 
-      protected EntityManager getEntityManager() {
-            EntityManager entityManager = emf.createEntityManager();
-            return entityManager;
-      }
-      protected Class<T> entityClass;
+    protected EntityManager getEntityManager() {
+        if (entityManager == null || !entityManager.isOpen()) {
+            entityManager = emf.createEntityManager();
+        }
+        return entityManager;
+    }
+    protected Class<T> entityClass;
 
-      public JpaGeneric() {
-            ParameterizedType genericSuperclass = (ParameterizedType) getClass()
-                    .getGenericSuperclass();
-            this.entityClass = (Class<T>) genericSuperclass
-                    .getActualTypeArguments()[0];
-      }
+    public JpaGeneric() {
+        ParameterizedType genericSuperclass = (ParameterizedType) getClass()
+                .getGenericSuperclass();
+        this.entityClass = (Class<T>) genericSuperclass
+                .getActualTypeArguments()[0];
+    }
 
-      public T create(T t) {
-            this.getEntityManager().persist(t);
-            return t;
-      }
+    public T create(T t) {
+        this.getEntityManager().persist(t);
+        return t;
+    }
 
-      public T read(PK id) {
-            return this.getEntityManager().find(entityClass, id);
-      }
+    public T read(PK id) {
+        return this.getEntityManager().find(entityClass, id);
+    }
 
-      public T update(T t) {
-            return this.getEntityManager().merge(t);
-      }
+    // alias to read()
+    public T findById(PK id) {
+        return read(id);
+    }
 
-      public void delete(T t) {
-            t = this.getEntityManager().merge(t);
-            this.getEntityManager().remove(t);
-      }
+    public T update(T t) {
+        return this.getEntityManager().merge(t);
+    }
 
-      public long getTotalCount() {
-            return (Long) getEntityManager().createQuery(
-                    "SELECT COUNT(*) FROM " + entityClass.getSimpleName())
-                    .getSingleResult();
-      }
+    public void delete(T t) {
+        t = this.getEntityManager().merge(t);
+        this.getEntityManager().remove(t);
+    }
 
+    // alias to getCount();
+    public long size() {
+        return getCount();
+    }
 
-      public Collection<T> findAll() {
-            return getEntityManager().createQuery(
-                    "SELECT e FROM " + entityClass.getSimpleName() + " e")
-                    .getResultList();
-      }
+    public long getCount() {
+        return (Long) getEntityManager().createQuery(
+                "SELECT COUNT(*) FROM " + entityClass.getSimpleName())
+                .getSingleResult();
+    }
 
-      /**
-       * inserts or updates an entity
-       *
-       * @param entity
-       * @return the persisted entity - might be a different object than the
-       * parameter
-       */
-      public T save(T entity) {
-            if (entity == null) {
-                  throw new IllegalArgumentException();
-            }
+    @SuppressWarnings("unchecked")
+    public Collection<T> findAll() {
+        return getEntityManager().createQuery(
+                "SELECT e FROM " + entityClass.getSimpleName() + " e")
+                .getResultList();
+    }
 
-            EntityManager em = getEntityManager();
-            assert em != null;
+    /**
+     * inserts or updates an entity
+     *
+     * check
+     * http://blog.xebia.com/2009/03/23/jpa-implementation-patterns-saving-detached-entities/
+     * for a discussion on saveOrUpdate() behaviour and merge()
+     *
+     * @param entity
+     * @return the persisted entity - migth be a diferent object than the
+     * parameter
+     *
+     * FAM[2013-04-29] - comentei a linha tx.rollback(); Lan�ava a seguinte
+     * exce��o: Exception in thread "main" java.lang.IllegalStateException:
+     * Exception Description: No transaction is currently active
+     */
+    public T save(T entity) {
+        if (entity == null) {
+            throw new IllegalArgumentException();
+        }
+
+        EntityManager em = getEntityManager();
+        assert em != null;
+        try {
+            // transaction will be rolled back if any exception occurs
+            EntityTransaction tx = em.getTransaction();
             try {
-                  // transaction will be rolled back if any exception occurs
-                  EntityTransaction tx = em.getTransaction();
-                  try {
-                        tx.begin();
-                        em.persist(entity);
-                        tx.commit();
-                  } catch (PersistenceException ex) {
-                        tx.rollback();
-                        // we need to set up a new transaction if persist raises an exception
-                        tx = em.getTransaction();
-                        tx.begin();
-                        entity = em.merge(entity);
-                        tx.commit();
-                  }
-            } finally {
-                  em.close();
+                tx.begin();
+                em.persist(entity);
+                tx.commit();
+            } catch (PersistenceException ex) {
+                // we need to set up a new transaction if persist raises an exception
+                tx = em.getTransaction();
+                tx.begin();
+                entity = em.merge(entity);
+                tx.commit();
             }
+        } finally {
+            em.close();
+        }
 
-            return entity;
-      }
+        return entity;
+    }
 
-      public List<T> all() {
-            // TODO check performance impact of this 'where' clause
-//            return match("1=1");
+    public List<T> all() {
+        // TODO check performance impact of this 'where' clause
+        return match("1=1");
 
+//        EntityManager em = getEntityManager();
+//        assert em != null;
+//
+//        String tableName = entityClass.getName(); //entityClass.getAnnotation(Table.class).name();
+//        Query q = em.createQuery("SELECT it FROM " + tableName + " it");
+//        List<T> all = q.getResultList();
+//        return all;
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<T> match(String where) {
         EntityManager em = getEntityManager();
         assert em != null;
 
         String tableName = entityClass.getSimpleName(); //entityClass.getAnnotation(Table.class).name();
-        Query q = em.createQuery("SELECT it FROM " + tableName + " it");
-        List<T> all = q.getResultList();
-        return all;
-      }
-
-//      public List<T> match(String where) {
-//            EntityManager em = getEntityManager();
-//            assert em != null;
-//
-//            //String tableName = entityClass.getName(); 
-//            String tableName = entityClass.getSimpleName();
-//            //entityClass.getAnnotation(Table.class).name();
-//
-//
-//            Query q = em.createQuery("SELECT it FROM " + tableName + " it WHERE " + where);
-//            List<T> some = q.getResultList();
-//
-////        
-////        CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
-////        cq.select(cq.from(ExpenseType.class));
-////       Query q = em.createQuery(cq);
-////        
-////        List<T> some = q.getResultList();
-//            return some;
-//      }
-}
+        Query q = em.createQuery("SELECT it FROM " + tableName + " it WHERE " + where);
+        List<T> some = q.getResultList();
+        return some;
+    }
+	}
